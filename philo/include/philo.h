c8a154d07be2653f1226be991bf4f4c0261bf57c
elianelang <eliane.lang@telefonica.com>
2022-07-04 00:41:28 -0300

    struct

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: ebresser <ebresser@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 22:58:57 by ebresser          #+#    #+#             */
-/*   Updated: 2022/07/03 23:00:41 by ebresser         ###   ########.fr       */
+/*   Updated: 2022/07/04 00:28:59 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
+
+#define N_PHILO 8
+
+typedef struct s_philo
+{
+    int id;
+    int *r_fork;
+    int *l_fork;
+    // pthread_mutex_t **mutex_r_fork;
+    // pthread_mutex_t **mutex_l_fork;
+} t_philo;
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,14 +6,52 @@
 /*   By: ebresser <ebresser@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 22:34:10 by ebresser          #+#    #+#             */
-/*   Updated: 2022/07/03 23:02:06 by ebresser         ###   ########.fr       */
+/*   Updated: 2022/07/04 00:38:32 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello from philo!");
+    char *s;
+    argc = argc - 1;
+    s = argv[0];
+    int i;
+    printf("Hello from %s!\n", s);
+    //----------------------------------
+    int fork[N_PHILO];
+    t_philo philo[N_PHILO];
+    // pthread_mutex_t mutex_fork[N_PHILO];
+
+    // init struct
+    i = 0;
+    while (i < N_PHILO)
+    {
+        fork[i] = i + 1;
+        philo[i].id = i + 1;
+        if (i + 1 != 1)
+        {
+            philo[i].r_fork = fork + i - 1;
+            philo[i].l_fork = fork + i;
+        }
+        else
+        {
+            philo[i].r_fork = fork + N_PHILO - 1;
+            philo[i].l_fork = fork + i;
+        }
+        i++;
+    }
+    i = 0;
+    while (i < N_PHILO)
+    {
+        printf("Philo %d:::::::::::\n", i + 1);
+        printf("____id: %d:\n", philo[i].id);
+        // printf("%d\n", *(fork + i));
+        printf("r_fork: %d:\n", *(philo[i].r_fork));
+        printf("l_fork: %d:\n", *(philo[i].l_fork));
+        i++;
+    }
+
     return (0);
 }
