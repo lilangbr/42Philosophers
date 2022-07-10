3491ba0777c4c6616ae54af31092edd7969e5f92
lilangbr <eblang@gmail.com>
2022-07-09 21:01:10 -0300

    3

--- philo/src/actions.c
+++ philo/src/actions.c
@@ -6,7 +6,7 @@
 /*   By: ebresser <ebresser@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/09 17:43:53 by ebresser          #+#    #+#             */
-/*   Updated: 2022/07/09 18:53:43 by ebresser         ###   ########.fr       */
+/*   Updated: 2022/07/09 20:46:56 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,18 +33,22 @@ void	release_forks(t_philo *philo)
 
 void	pick_up_forks(t_philo *philo)
 {
-	if (*philo->is_dead == FALSE && is_satiated(philo) == FALSE)
-	{
+	
 		pthread_mutex_lock(philo->left_fork);
-		pthread_mutex_lock(philo->printer);
-		output(duration(philo->simul_start), philo->id, PICK_LEFT_FORK, 0);
-		pthread_mutex_unlock(philo->printer);
+		if (*philo->is_dead == FALSE && is_satiated(philo) == FALSE)
+		{
+			pthread_mutex_lock(philo->printer);
+			output(duration(philo->simul_start), philo->id, PICK_LEFT_FORK, 0);
+			pthread_mutex_unlock(philo->printer);
+		}
 		pthread_mutex_lock(philo->right_fork);
-		pthread_mutex_lock(philo->printer);
-		output(duration(philo->simul_start), philo->id, PICK_RIGHT_FORK, 0);
-		pthread_mutex_unlock(philo->printer);
-		philo->last_meal = duration(philo->simul_start);
-	}
+		if (*philo->is_dead == FALSE && is_satiated(philo) == FALSE)
+		{
+			pthread_mutex_lock(philo->printer);
+			output(duration(philo->simul_start), philo->id, PICK_RIGHT_FORK, 0);
+			pthread_mutex_unlock(philo->printer);
+		}		
+		philo->last_meal = duration(philo->simul_start);	
 }
 
 int	thinking(t_philo *philo)
@@ -82,9 +86,12 @@ int	eating(t_philo *philo)
 		> philo->time_to_die && *philo->is_dead == FALSE)
 		return (FALSE);
 	pick_up_forks(philo);
-	pthread_mutex_lock(philo->printer);
-	output(duration(philo->simul_start), philo->id, EATING, 0);
-	pthread_mutex_unlock(philo->printer);
+	if (*philo->is_dead == FALSE && is_satiated(philo) == FALSE)
+	{	
+		pthread_mutex_lock(philo->printer);
+		output(duration(philo->simul_start), philo->id, EATING, 0);
+		pthread_mutex_unlock(philo->printer);
+	}
 	wait_time_in_ms(philo->time_to_eat);
 	release_forks(philo);
 	return (TRUE);
